f9f07380a07e9dcf37956c6e322c4038f095ed3b
MefAldemisov <alisha-omg@yandex.ru>
2021-08-18 17:52:32 +0300

    Style updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,19 @@ from app_python import create_app
 
 @pytest.fixture
 def app():
+    """
+    The 'app' instance generator
+    :return: the Flask instance with the app
+    """
     app = create_app()
     yield app
 
 
 @pytest.fixture
 def client(app):
-    """A test client for the app."""
+    """
+    The 'client' instance generator
+    :param app: the Flask app, which will be called by a client
+    :return: the Client instance
+    """
     return app.test_client()
