5b8e518ac777d5ec9a26b3523ddf3fad48bd132e
MefAldemisov <alisha-omg@yandex.ru>
2021-08-26 17:27:45 +0300

    Fixed error in test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,16 @@
 """
 This module is responsible the testing of `get_time` finction
 """
-from time import sleep
-from datetime import datetime
+from time import sleep, localtime
+from datetime import datetime, timedelta, timezone
 
 from app_python.main import get_time
 
 
 class FixedDatetime:
     """
-    This class is used to generate datetime objects with a fixed value
+    This class is used to generate datetime objects with a fixed value.
+    This time is set for UTC
     """
 
     def __init__(self, hours: int = 12,
@@ -20,23 +21,36 @@ class FixedDatetime:
         self.time_string = "{} {:0=2d}:{:0=2d}:{:0=2d}".\
             format(date, hours, minutes, seconds)
 
-        self.expected_time = "{:0=2d} hours {:0=2d} minutes {:0=2d} seconds".\
-            format(hours, minutes, seconds)
+    @staticmethod
+    def _format_expected(hours: int, minutes: int, seconds: int):
+        """
+        Format the string with expected outcome
+        :param hours: hours in the outcome
+        :param minutes: minutes in the outcome
+        :param seconds: seconds in outcome
+        :return: the string in format "xx hours xx minutes xx seconds"
+        """
+        return "{:0=2d} hours {:0=2d} minutes {:0=2d} seconds".format(hours, minutes, seconds)
 
     def generate_datetime(self) -> datetime:
         """
         The generator for a set of datetime objects with the same parameters
-        :return: the datetime with a given date and time
+        :return: the datetime with a given date and time at UTC format
         """
         datetime_obj = datetime.strptime(self.time_string, "%Y-%m-%d %H:%M:%S")
-        return datetime_obj
+        local_tz_offset = localtime().tm_gmtoff
+        local_tz_offset_delta = timedelta(0, local_tz_offset)
+        datetime_utc = datetime_obj + local_tz_offset_delta
+        # the last line updates the marker of timezone
+        return datetime_utc.replace(tzinfo=timezone.utc)
 
-    def get_expected_time(self) -> str:
+    def expected_time(self) -> str:
         """
         Returns the expected outcome of the `get_time` function
         :return: string in format "{:0=2d} hours {:0=2d} minutes {:0=2d} seconds"
         """
-        return self.expected_time
+        date_time = self.generate_datetime()
+        return self._format_expected(date_time.hour, date_time.minute, date_time.second)
 
 
 def test_get_time_expected_output():
@@ -45,8 +59,8 @@ def test_get_time_expected_output():
     Checks if the output is the in the same format, as expected
     """
     fixed_datetime = FixedDatetime()
-    moscow_time = get_time("Europe/Moscow", fixed_datetime.generate_datetime)
-    assert moscow_time == fixed_datetime.expected_time
+    utc_time = get_time("UTC", fixed_datetime.generate_datetime)
+    assert utc_time == fixed_datetime.expected_time()
 
 
 def test_get_time_different_tz():
