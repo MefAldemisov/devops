a12e50bd8101204e027976e28666cb07fa9a81d8
MefAldemisov <alisha-omg@yandex.ru>
2021-09-06 17:18:39 +0300

    Pinged-ponged with a host

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "instance" {
-  ami             = "ami-0ff338189efb7ed37"
+  ami             = "ami-0f0b4cb72cf3eadf3"
+  security_groups = ["SshGroup"]
   instance_type   = "t3.micro"
-  security_groups = ["default"]
   key_name        = "lab4"
   tags = {
     Name = "Web server by TerraForm"
   }
+
 }
 output "my-public-ip" {
   value = aws_instance.instance.public_ip
